9009be8855120b46de69944d42051051e5e97711
Antonella D'Anatro <adanatropintos@gmail.com>
2026-06-06 00:01:22 -0300

    updated_transformed_data

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 # TODO: Aquí debes escribir tu código
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title="Análisis dataset bancario", page_icon=":bank:", layout="wide")
+
+df = pd.read_csv("../data/processed/cleaned_data.csv", sep=",")
+
+col_numericas = [""]
+# Para ejecutar la app
+# streamlit run app.py
